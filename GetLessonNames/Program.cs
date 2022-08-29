0095819518bc6639bba69762d70f6aa8c5f804c6
BerkayUslu <mberkayuslu@outlook.com>
2022-08-29 14:35:13 +0300

    up

--- GetLessonNames/Program.cs
+++ GetLessonNames/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
 using ClosedXML.Excel;
 
 
